7d071b9819222b489151ec953fa96d5c5cfb9ab1
João Tomaz <joaotomas13@gmail.com>
2019-08-30 17:42:30 +0100

    feat(skill detailed): add skill detailed page base

---
+++ src/app/modules/public/pages/skills/skills-detailed/skills-detailed.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-skills-detailed',
+  templateUrl: './skills-detailed.component.html',
+  styleUrls: ['./skills-detailed.component.scss']
+})
+export class SkillsDetailedComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
